07eb92333c3834717f8c6c32d8549137eceafb0a
miyoon17 <153158257+miyoon17@users.noreply.github.com>
2024-01-29 18:00:12 +0900

    Issueの解消4~6 再修正版

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,7 +8,6 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    binding.pry
     Plan.create(plan_params)
     redirect_to action: :index
   end
@@ -36,7 +35,6 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      puts x
 
       wday_num = Date.today.wday + x
       
@@ -44,7 +42,7 @@ class CalendarsController < ApplicationController
       wday_num = wday_num -7
       end
 
-      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday =>  wdays[wday_num]}
+      days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans, wday: wdays[wday_num]}
       @week_days.push(days)
     end
 
