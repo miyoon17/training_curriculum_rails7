3beb509b2bc9c3e395333acdf5b5e61ce1dfcea1
miyoon17 <153158257+miyoon17@users.noreply.github.com>
2024-01-29 17:02:00 +0900

    Issueの解消4~6　修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -16,11 +16,12 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:calendars, :day).permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan)
   end
 
   def getWeek
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
+
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
@@ -35,13 +36,15 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
+      puts x
 
-      wday_num = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
-      if
-        wday_num = wday_num -7
+      wday_num = Date.today.wday + x
+      
+      if wday_num >= 7
+      wday_num = wday_num -7
       end
 
-      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => wdaysから値を取り出す記述}
+      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday =>  wdays[wday_num]}
       @week_days.push(days)
     end
 
